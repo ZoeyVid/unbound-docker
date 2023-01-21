7170df0553c7c2af8be13f4062961fb2c2a7e6f4
Zoey <zoey@z0ey.de>
2023-01-21 17:30:11 +0100

    Update health-check.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- health-check.sh
+++ health-check.sh
@@ -4,13 +4,13 @@
 if [ "$(dig example.com @127.0.0.1 | grep -E ".*\..*\..*\..*" | grep "example\.com\." | head -n 1 | cut -f6)" != "$(dig example.com @"$dns" | grep -E ".*\..*\..*\..*" | grep "example\.com\." | head -n 1 | cut -f6)" ]; then
 echo "dig failed, killing unbound..."
 kill "$(cat /usr/local/etc/unbound/unbound.pid)"
-exit 1
+#exit 1
 elif [ "$(dig example.com @127.0.0.1 | grep -E ".*\..*\..*\..*" | grep "example\.com\." | head -n 1 | cut -f6)" = "$(dig example.com @"$dns" | grep -E ".*\..*\..*\..*" | grep "example\.com\." | head -n 1 | cut -f6)" ]; then
-exit 0
+#exit 0
 else
 echo "dig succeeded and failed, killing unbound..."
 kill "$(cat /usr/local/etc/unbound/unbound.pid)"
-exit 1
+#exit 1
 fi
 
 echo "$dns"
