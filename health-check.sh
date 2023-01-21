ea6d1d676e3f439d7ce239ad1eb112d771d1e5f1
Zoey <zoey@z0ey.de>
2023-01-21 17:26:17 +0100

    Update health-check.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- health-check.sh
+++ health-check.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+# shellcheck disable=SC2154
 if [ "$(dig example.com @127.0.0.1 | grep -E ".*\..*\..*\..*" | grep "example\.com\." | head -n 1 | cut -f6)" != "$(dig example.com @"$dns" | grep -E ".*\..*\..*\..*" | grep "example\.com\." | head -n 1 | cut -f6)" ]; then
 echo "dig failed, killing unbound..."
-kill $(cat /usr/local/etc/unbound/unbound.pid)
+kill "$(cat /usr/local/etc/unbound/unbound.pid)"
 exit 1
 elif [ "$(dig example.com @127.0.0.1 | grep -E ".*\..*\..*\..*" | grep "example\.com\." | head -n 1 | cut -f6)" = "$(dig example.com @"$dns" | grep -E ".*\..*\..*\..*" | grep "example\.com\." | head -n 1 | cut -f6)" ]; then
 exit 0
