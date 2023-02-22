6d7fa02916c02a46680772cbe0ce2d19f053c9ce
Zoey <zoey@z0ey.de>
2023-02-22 16:01:11 +0100

    Update health-check.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- health-check.sh
+++ health-check.sh
@@ -4,11 +4,11 @@ if [ -z "$dns" ]; then
 export dns=94.140.14.14
 fi
 
-if [ "$(dig example.com @127.0.0.1 | grep -E ".*\..*\..*\..*" | grep "example\.com\." | head -n 1 | cut -f6)" != "$(dig example.com @"$dns" | grep -E ".*\..*\..*\..*" | grep "example\.com\." | head -n 1 | cut -f6)" ]; then
+if [ "$(dig example.com IN A +short @127.0.0.1 | head -n 1)" != "$(dig example.com IN A +short @"$dns" | head -n 1)" ]; then
 echo "dig failed, killing unbound..."
 kill "$(cat /usr/local/etc/unbound/unbound.pid)"
 exit 1
-elif [ "$(dig example.com @127.0.0.1 | grep -E ".*\..*\..*\..*" | grep "example\.com\." | head -n 1 | cut -f6)" = "$(dig example.com @"$dns" | grep -E ".*\..*\..*\..*" | grep "example\.com\." | head -n 1 | cut -f6)" ]; then
+elif [ "$(dig example.com IN A +short @127.0.0.1 | head -n 1)" = "$(dig example.com IN A +short @"$dns" | head -n 1)" ]; then
 exit 0
 else
 echo "dig succeeded and failed, killing unbound..."
