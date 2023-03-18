1ce0ff992fb894cd784723b54ad877f9b3f6cf37
Zoey <zoey@z0ey.de>
2023-03-18 17:32:45 +0100

    Delete health-check.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- health-check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "$(dig example.com IN A +short @127.0.0.1 | head -n 1)" = "$(dig example.com IN A +short +tls @"$dns" | head -n 1)" ]; then
-  exit 0
-else
-  echo "dig failed, killing unbound..."
-  kill "$(cat /usr/local/etc/unbound/unbound.pid)"
-  exit 1
-fi
